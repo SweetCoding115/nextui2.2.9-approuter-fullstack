bb3353b362b7545e86cfd9eadb2bbce6923c31b7
@ttractivenes$ <moriahbuckridge96115@gmail.com>
2023-12-29 20:50:51 +0800

    testing getProviders hook

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,16 +4,16 @@ import { getProviders } from "next-auth/react";
 export default async function SignIn() {
   // const providers: ReturnType<typeof getProviders> = (await getProviders()) || {};
   const providers = await getProviders();
-  if (providers)
-    return (
-      <div className="flex min-h-screen flex-col items-center p-24">
-        {Object.values(providers).map((provider) => {
-          return (
-            <div key={provider.id} className="[&:not(:first-child)]:mt-4">
-              <LoginButton auth={provider} />
-            </div>
-          );
-        })}
-      </div>
-    );
+  // if (providers)
+  return (
+    <div className="flex min-h-screen flex-col items-center p-24">
+      {Object.values(providers).map((provider) => {
+        return (
+          <div key={provider.id} className="[&:not(:first-child)]:mt-4">
+            <LoginButton auth={provider} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
